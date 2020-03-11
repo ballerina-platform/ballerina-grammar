2f96ea0c566061dd85d9d83f91d263461ec38749
Nipuna Marcus <nipunac@wso2.com>
2020-03-11 10:10:21 +0530

    Add syntax highlighting for let keyword

--- prism/prism-ballerina.js
+++ prism/prism-ballerina.js
@@ -7,7 +7,7 @@ Prism.languages.ballerina = {
     'boolean': /\b(?:true|false)\b/,
     'keyword': (new RegExp(
         '\\b(?:' +
-        'if|else|iterator|try|catch|finally|fork|join|all|some|while|foreach|in|throw|return|returns|break|' + 
+        'if|else|iterator|try|catch|finally|fork|join|all|some|while|foreach|in|throw|return|returns|break|let|' +
         'timeout|transaction|aborted|abort|committed|failed|retries|onretry|onabort|oncommit|next|bind|with|lengthof|typeof|enum|' +
         'import|version|public|private|attach|as|native|documentation|lock|record|' +
         'from|on|select|group|by|having|order|where|followed|insert|into|update|delete|set|for|window|query|forever|untaint|start|await|done|check' +
